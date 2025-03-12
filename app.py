778c9463f555a8f448ba3fd70789f204a8ff7469
ShrutiGopale <146646572+ShrutiGopale@users.noreply.github.com>
2025-03-13 03:50:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import re
 import nltk
 from nltk.tokenize import word_tokenize
-from sklearn.feature_extraction_text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
